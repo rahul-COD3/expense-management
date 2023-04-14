5b8d2d09087f9dc4b4eeba8b3e6f6ad0fdb4b4d6
rahulpromact <rahul@promactinfo.com>
2023-04-14 14:03:42 +0530

    initial commit

---
+++ src/EMS.EntityFrameworkCore/EntityFrameworkCore/EMSDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EMS.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class EMSDbContextFactory : IDesignTimeDbContextFactory<EMSDbContext>
+{
+    public EMSDbContext CreateDbContext(string[] args)
+    {
+        EMSEfCoreEntityExtensionMappings.Configure();
+
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<EMSDbContext>()
+            .UseSqlServer(configuration.GetConnectionString("Default"));
+
+        return new EMSDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../EMS.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
